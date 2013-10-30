cc52bb0a8187690af3c534c0db204b50d0fa4a30
yangzhang <v-yza@v-yza.(none)>
2013-10-30 14:18:04 +0800

    add minimum draws

---
+++ Algorithms/Discrete Mathematics/Minimum Draws/main.cpp
@@ -0,0 +1,19 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int Test;
+    cin >> Test;
+    for (; Test--; ) {
+        int n;
+        cin >> n;
+        cout << n + 1 << endl;
+    }
+    return 0;
+}
+
