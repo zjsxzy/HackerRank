95f3cc2411e8aced91aa611f296b94e58ccbd9d9
yangzhang <v-yza@v-yza.(none)>
2013-10-31 10:44:42 +0800

    handshake

---
+++ Algorithms/Discrete Mathematics/Handshake/main.cpp
@@ -0,0 +1,33 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+	int Test;
+	cin >> Test;
+	while (Test--) {
+		LL n;
+		cin >> n;
+		cout << n * (n - 1) / 2 << endl;
+	}
+	return 0;
+}
