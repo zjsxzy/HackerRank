bb901d1bb3c245596d50fa9f94877bc164957bc5
zhangyang <zjsxzy@gmail.com>
2014-07-21 13:02:26 +0800

    Maximizing XOR

---
+++ Algorithms/Warmup/Maximizing XOR/main.cpp
@@ -0,0 +1,35 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+	int l, r, ret = 0;
+	scanf("%d%d", &l, &r);
+	for (int i = l; i <= r; i++)
+		for (int j = i + 1; j <= r; j++)
+			ret = max(ret, i ^ j);
+	printf("%d\n", ret);
+	return 0;
+}
+
