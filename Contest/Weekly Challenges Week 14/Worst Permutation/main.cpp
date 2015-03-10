ce155e91bf70f8128a856f3d9de4b50367bbf11d
zhangyang <zjsxzy@gmail.com>
2015-03-10 10:53:41 +0800

    Weekly Challenges Week 14

---
+++ Contest/Weekly Challenges Week 14/Worst Permutation/main.cpp
@@ -0,0 +1,52 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+const int MAXN = 100000 + 5;
+int n, K;
+int num[MAXN], pos[MAXN];
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("in.txt", "r", stdin);
+#endif
+	scanf("%d%d", &n, &K);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", num + i);
+		pos[num[i]] = i;
+	}
+	for (int i = 0, j = 0; i < n && j < K; i++) {
+		if (num[i] != n - i) {
+			int x = i, y = pos[n - i];
+			// cout << x << " " << y << endl;
+			swap(num[x], num[y]);
+			swap(pos[num[x]], pos[num[y]]);
+			j++;
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		printf("%d%c", num[i], i == n - 1 ? '\n' : ' ');
+	}
+	return 0;
+}
+
