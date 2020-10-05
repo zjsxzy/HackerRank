91514ed7f021ecb1d5bd64dd4e91fbced9f3c5a0
zjsxzy <zjsxzy@gmail.com>
2020-10-05 10:40:07 +0800

    add a very big sum

---
+++ Algorithms/Warmup/A Very Big Sum/main.cpp
@@ -0,0 +1,38 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int n;
+    cin >> n;
+    long long sum = 0;
+    for (int i = 0; i < n; i++) {
+        long long x;
+        cin >> x;
+        sum += x;
+    }
+    cout << sum << endl;
+    return 0;
+}
+
