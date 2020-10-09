2bdbfec560ea5d75aad802de56a1dba1726a4527
zjsxzy <zjsxzy@gmail.com>
2020-10-09 21:02:20 +0800

    compare the triplets

---
+++ Algorithms/Warmup/Compare the Triplets/main.cpp
@@ -0,0 +1,40 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    vector<int> a(3), b(3);
+    cin >> a[0] >> a[1] >> a[2];
+    cin >> b[0] >> b[1] >> b[2];
+    int alice = 0, bob = 0;
+    if (a[0] > b[0]) alice++;
+    else if (a[0] < b[0]) bob++;
+    if (a[1] > b[1]) alice++;
+    else if (a[1] < b[1]) bob++;
+    if (a[2] > b[2]) alice++;
+    else if (a[2] < b[2]) bob++;
+    cout << alice << " " << bob << endl;
+    return 0;
+}
+
