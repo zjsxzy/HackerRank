cc3ac0b663a0e446671b770cf3b58440904dd494
zhangyang <zjsxzy@gmail.com>
2020-10-13 17:38:20 +0800

    add diagonal difference

---
+++ Algorithms/Warmup/Diagonal Difference/main.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int n;
+    cin >> n;
+    vector<vector<int> > arr(n, vector<int>(n, 0));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    int sum1 = 0, sum2 = 0;
+    for (int i = 0; i < n; i++) {
+        sum1 += arr[i][i];
+        sum2 += arr[i][n - i - 1];
+    }
+    cout << abs(sum1 - sum2) << endl;
+    return 0;
+}
+
