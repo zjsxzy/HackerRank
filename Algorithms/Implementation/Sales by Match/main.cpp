b2dc15f8bc1f50308fdd9cccaa10648c38a054fc
zjsxzy <zjsxzy@gmail.com>
2020-10-21 21:21:28 +0800

    add sales by match

---
+++ Algorithms/Implementation/Sales by Match/main.cpp
@@ -0,0 +1,42 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int n;
+    cin >> n;
+    map<int, int> cnt;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        cnt[x]++;
+    }
+    int res = 0;
+    for (auto it : cnt) {
+        res += it.second / 2;
+    }
+    cout << res << endl;
+    return 0;
+}
+
