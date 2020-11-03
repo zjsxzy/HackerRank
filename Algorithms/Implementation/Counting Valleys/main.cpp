031daba81119341f0044bb762fca9b3e10726c25
zjsxzy <zjsxzy@gmail.com>
2020-11-03 23:23:35 +0800

    add counting valleys

---
+++ Algorithms/Implementation/Counting Valleys/main.cpp
@@ -0,0 +1,43 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int n;
+    cin >> n;
+    string path;
+    cin >> path;
+    int level = 0, res = 0;
+    for (int i = 0; i < n; i++) {
+        if (path[i] == 'U') {
+            level++;
+        } else {
+            if (level == 0) res++;
+            level--;
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
+
