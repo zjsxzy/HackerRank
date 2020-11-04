5d76e260ceff270dfe0cc39898b1172fbfa904b5
zjsxzy <zjsxzy@gmail.com>
2020-11-04 23:01:43 +0800

    add cutting paper squares

---
+++ Mathematics/Fundamentals/Cutting Paper Squares/main.cpp
@@ -0,0 +1,36 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    if (n == 1 || m == 1) {
+        cout << n - 1 + m - 1 << endl;
+    } else {
+        cout << (LL)n * (m - 1) + n - 1 << endl;
+    }
+    return 0;
+}
+
