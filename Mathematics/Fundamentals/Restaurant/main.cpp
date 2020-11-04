cd9473b81f41d6d613c33c9afea946f3188e2b41
zjsxzy <zjsxzy@gmail.com>
2020-11-04 20:58:06 +0800

    add restaurant

---
+++ Mathematics/Fundamentals/Restaurant/main.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int gcd(int a, int b) {
+    return a ? gcd(b % a, a) : b;
+}
+
+int main() {
+    int ts;
+    cin >> ts;
+    while (ts--) {
+        int l, b;
+        cin >> l >> b;
+        int g = gcd(l, b);
+        cout << l * b / g / g << endl;
+    }
+    return 0;
+}
+
