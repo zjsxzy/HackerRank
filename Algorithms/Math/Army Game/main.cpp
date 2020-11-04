5666c7a0b3a0a7147dd6fe0351d22f41fb942736
zjsxzy <zjsxzy@gmail.com>
2020-11-04 20:35:09 +0800

    add army game

---
+++ Algorithms/Math/Army Game/main.cpp
@@ -0,0 +1,32 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    cout << (n / 2 + n % 2) * (m / 2 + m % 2) << endl;
+    return 0;
+}
+
