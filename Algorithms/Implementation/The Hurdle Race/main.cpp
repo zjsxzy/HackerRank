c1306f17d045bdbc17d6c6c859242f7b803d3703
Zhang Yang <zjsxzy@gmail.com>
2020-11-04 09:56:06 +0800

    add the hurdle race

---
+++ Algorithms/Implementation/The Hurdle Race/main.cpp
@@ -0,0 +1,35 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int res = 0;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        if (x > k) res = max(res, x - k);
+    }
+    cout << res << endl;
+    return 0;
+}
