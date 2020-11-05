e39bfdcc229e54c1a0408f95ff559336aa5203b9
Zhang Yang <zjsxzy@gmail.com>
2020-11-05 17:46:03 +0800

    add utopian tree

---
+++ Algorithms/Implementation/Utopian Tree/main.cpp
@@ -0,0 +1,38 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int ts;
+    cin >> ts;
+    while (ts--) {
+        int n;
+        cin >> n;
+        LL res = 1;
+        for (int i = 1; i <= n; i++) {
+            if (i & 1) res *= 2;
+            else res++;
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
