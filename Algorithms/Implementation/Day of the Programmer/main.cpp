71c9420976b0e52c4121c5a15426f4feea5b51a5
Zhang Yang <zjsxzy@gmail.com>
2020-11-05 09:46:02 +0800

    day of the programmer

---
+++ Algorithms/Implementation/Day of the Programmer/main.cpp
@@ -0,0 +1,37 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int year;
+    cin >> year;
+    if (year == 1918) {
+        cout << "26.09.1918" << endl;
+    } else {
+        if ((year < 1918 && year % 4 == 0) || (year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+            cout << "12.09." << year << endl;
+        } else {
+            cout << "13.09." << year << endl;
+        }
+    }
+    return 0;
+}
