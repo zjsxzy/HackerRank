fcd91876918d6340626bc1e3310fe868e0be0bae
zjsxzy <zjsxzy@gmail.com>
2020-11-07 14:35:45 +0800

    add repeated string

---
+++ Algorithms/Implementation/Repeated String/main.cpp
@@ -0,0 +1,42 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    string s;
+    cin >> s;
+    LL n;
+    cin >> n;
+    int m = n % (int)s.size();
+    LL cnt = 0, tot = 0;
+    for (int i = 0; i < (int)s.size(); i++) {
+        if (s[i] == 'a') {
+            cnt++;
+            if (i < m) tot++;
+        }
+    }
+    cout << n / (int)s.size() * cnt + tot << endl;
+    return 0;
+}
+
