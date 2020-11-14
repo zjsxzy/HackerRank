47b0d0b1b4d522f82cfb14321e67511fea38f9cf
zjsxzy <zjsxzy@gmail.com>
2020-11-14 10:33:29 +0800

    add

---
+++ Algorithms/String/Making Anagrams/Beautiful Binary String/main.cpp
@@ -0,0 +1,41 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cassert>
+#include <climits>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+#define FOREACH(e,x) for(__typeof(x.begin()) e=x.begin();e!=x.end();++e)
+typedef long long LL;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int res = 0;
+    for (int i = 0; i + 2 < s.size(); i++) {
+        if (s.substr(i, 3) == "010") {
+            res++;
+            i += 2;
+        }
+    }
+    cout << res << endl;
+    return 0;
+}
+
