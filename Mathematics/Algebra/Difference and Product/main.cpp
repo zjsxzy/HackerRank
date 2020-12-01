9968f324d346d52e4947bbe28db6f98ae50ba03b
zjsxzy <zjsxzy@gmail.com>
2020-12-01 21:04:14 +0800

    update

--- Mathematics/Algebra/Difference and Product/main.cpp
+++ Mathematics/Algebra/Difference and Product/main.cpp
@@ -1,20 +1,36 @@
-#include <bits/stdc++.h>
-
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <queue>
+#include <stack>
+#include <bitset>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <sstream>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
 using namespace std;
+#define PB push_back
+#define MP make_pair
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
 
 void solve() {
-	int d, p;
+    long long d, p;
 	cin >> d >> p;
-	int sign = 1;
 	if (d < 0) {
 		cout << 0 << endl;
 		return;
-	}
-	if (p == 0) {
-		cout << 4 << endl;
-		return;
-	}
-    long long t = (long long)d * d + 4LL * p;
+	} else if (d == 0 && p == 0) {
+        cout << 1 << endl;
+        return;
+    }
+    long long t = d * d + 4LL * p;
     if ((long long)sqrt(t) * (long long)sqrt(t) == t) {
         int res = 2;
         if (d && t) res += 2;
