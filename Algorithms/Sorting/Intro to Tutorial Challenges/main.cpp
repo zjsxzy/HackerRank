7643ce699d4fedbb3f7ff40d9872686e792dfdab
Zhang Yang <zjsxzy@gmail.com>
2021-05-13 11:23:28 +0800

    add intro to tutorial challenges

---
+++ Algorithms/Sorting/Intro to Tutorial Challenges/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+void solve() {
+}
+
+int main() {
+    solve();
+    return 0;
+}
+
