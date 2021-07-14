006694ed33c9a39ab5c565add839747337633f8b
zjsxzy <zjsxzy@gmail.com>
2021-07-14 23:42:23 +0800

    maximum xor

---
+++ Interview Preparation Kit/Miscellaneous/Maximum Xor/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+struct Trie {
+};
+
+void solve() {
+    int n, m;
+    cin >> n;
+    vector<int> arr(n);
+    for (auto &x: arr) {
+        cin >> x;
+    }
+    cin >> m;
+    for (int i = 0; i < n; i++) {
+        int q;
+        cin >> q;
+    }
+}
+
+int main() {
+    solve();
+    return 0;
+}
+
