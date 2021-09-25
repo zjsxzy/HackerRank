d36895a1f05ed3a0a122c85845e378bdce7c740b
zjsxzy <zjsxzy@gmail.com>
2021-09-25 13:38:01 +0800

    recusion: fibonacci numbers

---
+++ Interview Preparation Kit/Recusion and Backtracking/Recursion_ Fibonacci Numbers/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PB push_back
+#define SZ(v) ((int)(v).size())
+#define abs(x) ((x) > 0 ? (x) : -(x))
+typedef long long LL;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> f(n + 1);
+    f[0] = 0; f[1] = 1;
+    for (int i = 2; i <= n; i++) {
+        f[i] = f[i - 1] + f[i - 2];
+    }
+    cout << f[n] << endl;
+}
+
+int main() {
+    solve();
+    return 0;
+}
+
