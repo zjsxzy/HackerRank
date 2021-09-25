4c85ed37e84dd482c2cbb56fdc81ae8bc59b3326
zjsxzy <zjsxzy@gmail.com>
2021-09-25 15:31:08 +0800

    recursive digit sum

---
+++ Interview Preparation Kit/Recusion and Backtracking/Recursive Digit Sum/main.py
@@ -0,0 +1,17 @@
+def super_digit(n, k):
+    digits = map(int, list(n))
+    return get_super_digit(str(sum(digits) * k))
+
+def get_super_digit(p):
+    if len(p) == 1:
+        return int(p)
+    else:
+        digits = map(int, list(p))
+        return get_super_digit(str(sum(digits)))
+
+if __name__ == '__main__':
+    nk = input().split()
+    n = nk[0]
+    k = int(nk[1])
+    res = super_digit(n, k)
+    print(res)
