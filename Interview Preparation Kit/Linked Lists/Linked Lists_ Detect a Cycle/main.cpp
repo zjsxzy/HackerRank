b9cc49eec55091e9f252ac5480535b38b86b1922
Zhang Yang <zjsxzy@gmail.com>
2021-09-27 16:24:07 +0800

    linked lists: detect a cycle

---
+++ Interview Preparation Kit/Linked Lists/Linked Lists_ Detect a Cycle/main.cpp
@@ -0,0 +1,10 @@
+bool has_cycle(Node* head) {
+    Node* slow = head;
+    Node* fast = head;
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+        if (slow == fast) return true;
+    }
+    return false;
+}
